854d274b92d2d25de22a626ae36ae3251fc1daad
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-11-27 14:35:34 -0800

    init upload of main file

---
+++ Project/main.py
@@ -0,0 +1,34 @@
+# Brianna Atayan -  - batayan@ucsc.edu
+# Colin Maher    - - csmaher@ucsc.edu
+# Lily Nguyen    - 1596857 - lnguye78@ucsc.edu
+
+import argparse
+
+# default file path for training data
+TRAINING_DATA_PATH = "train.csv"
+# default file path for testing data
+TESTING_DATA_PATH = "test.csv"
+# default file path for output
+OUTPUT_PATH = "output.csv"
+
+
+def main():
+    # read in training data
+    parser = argparse.ArgumentParser(description="program description")
+    parser.add_argument("--test", dest = "testFile", default = TESTING_DATA_PATH, \
+            type = str, help = "the path to the .csv file containing the test data")
+    parser.add_argument("--out", dest = "outFile", default = OUTPUT_PATH, \
+            type = str, help = "the path of the output file")
+    args = parser.parse_args()
+    print(args.testFile)
+
+    # preprocessing
+
+    # feature extraction
+
+    # training - send to different algorithms
+
+    # test
+
+if __name__ == '__main__':
+    main()

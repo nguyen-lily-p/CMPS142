576c032f73cfbd619cb0604e93e3c5db8fc86bb9
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-11-27 22:17:35 -0800

    added code to read training data into pandas dataframe

--- Project/main.py
+++ Project/main.py
@@ -1,8 +1,8 @@
-# Brianna Atayan -  - batayan@ucsc.edu
-# Colin Maher    - - csmaher@ucsc.edu
+# Brianna Atayan - 1632743 - batayan@ucsc.edu
+# Colin Maher    - 1432169 - csmaher@ucsc.edu
 # Lily Nguyen    - 1596857 - lnguye78@ucsc.edu
 
-import argparse
+import argparse, pandas, sys
 
 # default file path for training data
 TRAINING_DATA_PATH = "train.csv"
@@ -14,21 +14,41 @@ OUTPUT_PATH = "output.csv"
 
 def main():
     # read in command-line arguments, if any
-    parser = argparse.ArgumentParser(description="program description")
-    parser.add_argument("--test", dest = "testFile", default = TESTING_DATA_PATH, \
-            type = str, help = "the path to the .csv file containing the test data")
+    parser = argparse.ArgumentParser(description = "program to predict the "
+            "sentiment of a given phrase")
+    parser.add_argument("--train", dest = "trainFile", \
+            default = TRAINING_DATA_PATH, type = str, \
+            help = "the path to the .csv file containing the training data")
+    parser.add_argument("--test", dest = "testFile", \
+            default = TESTING_DATA_PATH, type = str, \
+            help = "the path to the .csv file containing the test data")
     parser.add_argument("--out", dest = "outFile", default = OUTPUT_PATH, \
             type = str, help = "the path of the output file")
     args = parser.parse_args()
-    print(args.testFile)
 
-    # read in training data
+
+    # read training data into a pandas dataframe
+    try:
+        train_data_df = pandas.read_csv(args.trainFile)
+    except FileNotFoundError:
+        print("Error: File does not exist. File must be of type csv")
+        sys.exit(1)
+    except:
+        print("Error: Unknown error occurred trying to read train data file")
+        sys.exit(1)
+
+    # make indexing start at 1
+    train_data_df.index = train_data_df.index + 1
+
 
     # preprocessing
 
+
     # feature extraction
 
+
     # training - send to different algorithms
+
 
     # test
 

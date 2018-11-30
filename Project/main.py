8e4f06f447027a0ad8d7fc381ab4bd5b0cb25f3d
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-11-29 19:30:54 -0800

    removed preprocessing op that removed stop-words, optimized pre-processing code (a little bit)

--- Project/main.py
+++ Project/main.py
@@ -13,30 +13,29 @@ TESTING_DATA_PATH = "test.csv"
 OUTPUT_PATH = "output.csv"
 
 def preprocess(phrase_df):
+"""
+Performs pre-processing operations on the text data.
+Converts strings to all lowercase, tokenizes the strings into words, removes
+    punctuation, removes non-alphabetic tokens, stems the tokens.
+Argument should be a pandas Series of strings
+Returns the pre-processed text data as a Series of lists of strings
+"""
     phrase_df = phrase_df.str.lower() # convert strings to lowercase
     phrase_df = phrase_df.str.strip() # remove leading/trailing whitespace
     phrase_df = phrase_df.str.split(' ') # tokenize into words
     
     mapping = str.maketrans('', '', string.punctuation)
-    stop_words = set(nltk.corpus.stopwords.words('english'))
-    
-    for i in range(0, phrase_df.size ):
-        # remove punctuation from tokens
-        phrase_df[i] = [token.translate(mapping) for token in phrase_df[i]]
-        # remove non-alphabetic tokens
-        phrase_df[i] = [token for token in phrase_df[i] if token.isalpha()]
-        # remove stop-words
-        phrase_df[i] = [token for token in phrase_df[i] \
-                if not token in stop_words]
-        # stem the words
-        phrase_df[i] = [nltk.stem.porter.PorterStemmer().stem(token) \
-                for token in phrase_df[i]]
+
+    # remove punctuation, remove non-alphabetic tokens, stem tokens
+    for i in range(0, phrase_df.size):
+        phrase_df[i] = [nltk.stem.porter.PorterStemmer().stem(token.translate(mapping)) \
+                for token in phrase_df[i] if token.translate(mapping).isalpha()]
 
     return phrase_df
 
 
 def main():
-    # read in command-line arguments, if any
+    #### read in command-line arguments, if any ####
     parser = argparse.ArgumentParser(description = "program to predict the "
             "sentiment of a given phrase")
     parser.add_argument("--train", dest = "trainFile", \
@@ -50,7 +49,7 @@ def main():
     args = parser.parse_args()
 
 
-    # read training data into a pandas dataframe
+    #### read training data into a pandas dataframe ####
     try:
         train_data_df = pandas.read_csv(args.trainFile)
     except FileNotFoundError:
@@ -61,10 +60,7 @@ def main():
         sys.exit(1)
 
 
-    # preprocessing
-    print("\n***PHRASE DATA BEFORE PREPROCESSING***")
-    print(train_data_df["Phrase"])
-
+    #### preprocessing ####
     train_data_df["Phrase"] = preprocess(train_data_df["Phrase"])
 
     # remove instances with empty phrase list after preprocessing
@@ -75,11 +71,8 @@ def main():
 
         index += 1
 
-    print("\n***PHRASE DATA AFTER PREPROCESSING***")
-    print(train_data_df["Phrase"])
 
-
-    # feature extraction
+    #### feature extraction ####
 
 
     # training - send to different algorithms

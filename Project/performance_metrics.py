6673ca89f32866376372154176c33d88211d295e
Lily Nguyen <nguyen.lily.p@gmail.com>
2018-12-07 22:11:59 -0800

    added function comments

--- Project/performance_metrics.py
+++ Project/performance_metrics.py
@@ -6,6 +6,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 
 def get_performance_train(model, features, labels, file, class_performance = False):
+    """
+        Get the performance metrics of a model on the direct, entire training set.
+        Parameters are the ensemble model, the extracted feature set of the training data,
+            the labels of the instances, the file to write results to, and whether
+            to print the statistics for the individual classes/labels.
+    """
     print("in get_performance")
     file.write("\n******************** TRAINING SET ********************\n")
     
@@ -14,8 +20,10 @@ def get_performance_train(model, features, labels, file, class_performance = Fal
     for classifier in classifierList:
         get_performance_train_helper(classifier, features, labels, file, class_performance)
     
+    # performance of the ensemble classifier
     get_performance_train_helper(model, features, labels, file, class_performance)
     
+    # count of the instances in each class
     predictions = model.predict(features)
     class_metrics = sklearn.metrics.precision_recall_fscore_support(labels, predictions)
     file.write("\n\tClass Count:\n")
@@ -25,33 +33,45 @@ def get_performance_train(model, features, labels, file, class_performance = Fal
         
     file.write("\n************************************************************\n")
 
+    
 def get_performance_train_helper(classifier, features, labels, file, class_performance):
+    """
+        Helper function for get_performance_train()
+        Prints the accuracy, precision, recall, F1-score, and confusion matrix of a classifier.
+        If class_performance = True, also prints the precision, recall, and F1-score
+            for each class/label.
+    """
     name = str(type(classifier))[16:-2]
     file.write("\n********** CLASSIFIER: " + name + "**********\n")
     
     predictions = classifier.predict(features)
     class_metrics = sklearn.metrics.precision_recall_fscore_support(labels, predictions)
     
+    # accuracy
     file.write("\tAccuracy: " + str(sklearn.metrics.accuracy_score(labels, predictions)) + "\n")
     
+    # precision
     file.write("\tPrecision: " + str(sklearn.metrics.precision_score(labels, predictions, average = "weighted")) + "\n")
     if class_performance:
         for i in range(0, 5):
             file.write("\t\tClass " + str(i) + ": ")
             file.write(str(class_metrics[0][i]) + "\n")
 
+    # recall
     file.write("\tRecall: " + str(sklearn.metrics.recall_score(labels, predictions, average = "weighted")) + "\n")
     if class_performance:
         for i in range(0, 5):
             file.write("\t\tClass " + str(i) + ": ")
             file.write(str(class_metrics[1][i]) + "\n")
 
+    # f1-score
     file.write("\tF-1 Score: " + str(sklearn.metrics.f1_score(labels, predictions, average = "weighted")) + "\n")
     if class_performance:
         for i in range(0, 5):
             file.write("\t\tClass " + str(i) + ": ")
             file.write(str(class_metrics[2][i]) + "\n")
    
+    # confusion matrix
     file.write("\tConfusion Matrix:\n")
     conf_matrix = sklearn.metrics.confusion_matrix(labels, predictions)
     for row in conf_matrix:
@@ -59,13 +79,19 @@ def get_performance_train_helper(classifier, features, labels, file, class_perfo
         
 
 def get_performance_cv(model, features, labels, file, cv_val = 5):
-    
+    """
+        Get the performance metrics of a model using cross validation.
+        Parameters are the ensemble model, the extracted feature set of the training data,
+            the labels of the instances, the file to write results to, and k-fold number.
+        NOTE: accuracy, precision, and recall metrics are currently commented out because
+            program takes too long to run when computing all of them.
+    """
     file.write("\n******************** CROSS VALIDATION (CV = " + str(cv_val) + ") ********************\n")
     
     classifierList = model.estimators_
     classifierList.append(model) # add VotingClassifier to list
     
-    ### output performance of individual classifiers and ensemble classifier ###
+    # output performance of individual classifiers and ensemble classifier
     for classifier in classifierList:
         name = str(type(classifier))[16:-2]
         file.write("\n********** CLASSIFIER: " + name + "**********\n")
